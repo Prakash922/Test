e7c360b55d02365322ff1c7f92b1b3c6d66bf4ce
Prakash922 <76912886+Prakash922@users.noreply.github.com>
2021-01-03 21:55:17 +0530

    Add files via upload

---
+++ condition1.py
@@ -0,0 +1,15 @@
+'''Conditional Statements:
+-----------------------
+if condition
+if else condition
+if elif condition
+if elif else condition
+nested condition
+'''
+
+if(True):
+    print('apple')
+else:
+    print('banana')
+    
+
